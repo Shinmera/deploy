b395ad839ee12ef15b3380037f22a01ad1387158
Shinmera <shinmera@tymoon.eu>
2017-06-06 19:23:53 +0200

    Export some stuff.

--- package.lisp
+++ package.lisp
@@ -9,4 +9,32 @@
   (:nicknames #:org.shirakumo.deploy)
   (:use #:cl)
   ;; deploy.lisp
-  (:export))
+  (:export
+   #:*data-location*
+   #:quit
+   #:deploy-op)
+  ;; library.lisp
+  (:export
+   #:*system-source-directories*
+   #:list-libraries
+   #:ensure-library
+   #:library
+   #:library-system
+   #:library-sources
+   #:library-path
+   #:library-dont-load-p
+   #:library-dont-copy-p
+   #:possible-pathnames
+   #:find-source-file
+   #:library-name
+   #:open-library
+   #:close-library
+   #:library-open-p
+   #:define-library)
+  ;; toolkit.lisp
+  (:export
+   #:status
+   #:env-set-p
+   #:redirect-output
+   #:runtime-directory
+   #:copy-directory-tree))
