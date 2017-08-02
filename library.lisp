8a8cf38dc91edca14511a78732b450f929661ba5
Shinmera <shinmera@tymoon.eu>
2017-08-02 16:00:07 +0200

    Add automatically coercing methods to library functions

--- library.lisp
+++ library.lisp
@@ -47,6 +47,9 @@
           (resolve-cffi-spec (slot-value library 'cffi::spec))
           (list (make-lib-pathname (format NIL "*~(~a~)*" (library-name library))))))
 
+(defmethod possible-pathnames (library)
+  (possible-pathnames (ensure-library library)))
+
 (defmethod possible-directories ((library library))
   ;; FIXME: Maybe use ld.so.cache
   (remove NIL
@@ -63,6 +66,9 @@
                   #+linux (env-paths "LD_LIBRARY_PATH")
                   #+darwin (env-paths "DYLD_LIBRARY_PATH"))))
 
+(defmethod possible-directories (library)
+  (possible-directories (ensure-library library)))
+
 (defmethod find-source-file ((library library))
   (let ((sources (possible-directories library)))
     (dolist (path (possible-pathnames library))
@@ -73,6 +79,9 @@
               do (when (uiop:file-exists-p file)
                    (return-from find-source-file file)))))))
 
+(defmethod find-source-file (library)
+  (find-source-file (ensure-library library)))
+
 (defmethod shared-initialize :after ((library library) slots &key)
   (unless (library-path library)
     (setf (library-path library) (find-source-file library))))
@@ -80,15 +89,27 @@
 (defmethod library-name ((library library))
   (cffi:foreign-library-name library))
 
+(defmethod library-name (library)
+  (library-name (ensure-library library)))
+
 (defmethod open-library ((library library))
   (cffi:load-foreign-library (library-name library)))
+
+(defmethod open-library (library)
+  (open-library (ensure-library library)))
 
 (defmethod close-library ((library library))
   (cffi:close-foreign-library
    (library-name library)))
 
+(defmethod close-library (library)
+  (close-library (ensure-library library)))
+
 (defmethod library-open-p ((library library))
   (cffi:foreign-library-loaded-p library))
+
+(defmethod library-open-p (library)
+  (library-open-p (ensure-library library)))
 
 (defmacro define-library (name &body initargs)
   `(change-class (cffi::get-foreign-library ',name)
